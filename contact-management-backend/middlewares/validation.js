e9fd9326f99315009ab49f7e75720980cfa39cf8
ChefnCoder <tanmay2020anand@gmail.com>
2024-11-16 15:23:55 +0530

    env update

--- contact-management-backend/middlewares/validation.js
+++ contact-management-backend/middlewares/validation.js
@@ -6,7 +6,7 @@ const validateContactInput = [
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        next();
+        next(); 
     },
 ];
 
